9a7297ec9e34b06a3b5b33b5919f0510b32dcd6d
siratl <elishubax@gmail.com>
2019-03-08 11:05:26 -0800

    MVP and STRETCH DONE: Routes written

--- server.js
+++ server.js
@@ -4,8 +4,62 @@ const server = express();
 
 server.use(express.json());
 
+let gamesData = [];
+
+let startIndex = 0;
+
+const increment = () => {
+  return (startIndex += 1);
+};
+
 server.get('/', (req, res) => {
-  server.send(`***** Server Running ****`);
+  res.send(`***** Server Running *****`);
+});
+
+//**************** POST ENPOINT *************/
+server.post('/games', (req, res) => {
+  const { title, genre, releaseYear } = req.body;
+  const result = { id: increment(), title, genre, releaseYear };
+
+  if (!title || !genre) {
+    res
+      .status(422)
+      .json({ message: 'The title and genre fields are required.' });
+  } else {
+    const duplicateTitle = gamesData.find(game => game.title === title);
+
+    if (duplicateTitle) {
+      res
+        .status(405)
+        .json({ message: 'A title with the specified name already exists.' });
+    } else {
+      gamesData.push(result);
+      res.status(201).json({ message: 'Game added sucessfully.', result });
+    }
+  }
+});
+
+//**************** GET ENPOINT *************/
+server.get('/games', (req, res) => {
+  res.json(gamesData);
+});
+
+server.get('/games/:id', (req, res) => {
+  const { id } = req.params;
+  const gameId = gamesData.find(game => game.id == id);
+
+  if (gameId) {
+    res.json(gameId);
+  } else {
+    res.status(404).json({ message: `Game with id: ${id} not found.` });
+  }
+});
+
+server.delete('/games/:id', (req, res) => {
+  const { id } = req.params;
+  const gameId = gamesData.filter(game => game.id != id);
+
+  res.json({ message: `Game Deleted!` });
 });
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -13,10 +13,22 @@ describe('POST /games', () => {
       })
       .then(res => expect(res.status).toBe(201));
   });
+
   it('should return status code 422 if required information incomplete', () => {
     request(server)
       .post('/games')
       .then(res => expect(res.status).toBe(422));
+  });
+
+  it('should validate that game title is unique', () => {
+    return request(server)
+      .post('/games')
+      .send({
+        title: 'Pacman', // required
+        genre: 'Arcade', // required
+        releaseYear: 1980, // not required
+      })
+      .then(res => expect(res.status).toBe(405));
   });
 });
 
@@ -27,6 +39,23 @@ describe('GET /games', () => {
       .then(res => {
         expect(res.status).toBe(200);
         expect(res.type).toBe('application/json');
+        expect(res.body).toEqual(expect.arrayContaining([]));
       });
+  });
+});
+
+describe('GET /games/:id', () => {
+  it('should return status code 404 if game not found', () => {
+    return request(server)
+      .get('/games/500')
+      .then(res => expect(res.status).toBe(404));
+  });
+});
+
+describe('DELETE /games/:id', () => {
+  it('should delete game with corresponding id', () => {
+    return request(server)
+      .delete('/games/3')
+      .then(res => expect(res.status).toBe(200));
   });
 });
